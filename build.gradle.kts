38d47d2e486151ff839ba016138fb4810814aaf2
Andrew Miller <andrewm192@outlook.com>
2023-01-08 00:21:57 -0500

    adjustments from dependency analysis again

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("com.autonomousapps.dependency-analysis") version "1.18.0"
+}
+
+dependencies {
+    repositories {
+        mavenCentral()
+    }
+}
